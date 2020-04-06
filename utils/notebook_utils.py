cd5f36d35f0f31047bed53c58ff998526e99c00b
Yeo Hui Fang <hfy@activeviam.com>
2020-04-06 16:54:36 +0800

    remove example from function doc

--- utils/notebook_utils.py
+++ utils/notebook_utils.py
@@ -6,30 +6,9 @@ def download_source(cwd: str, url: str, filename: str):
     """This function downloads data file from the given url to the working directory (cwd).
 
     Args:
-        cwd (string): current working directory.
-        url (string): url for the source file
-        filename (string): filename of the downloaded file
-
-    Examples:
-        In the notebook's working directory, if the data does not exists:
-        
-        >>> cwd = os.getcwd()
-        >>> download_source(
-                cwd, 
-                'http://data.atoti.io/notebooks/collateral-shortfall-monitoring/assets_positions.csv', 
-                "assets_positions.csv"
-            )
-        100% [..................................................................................] 142 / 142
-        
-        If the data is already downloaded to the working directory: 
-        
-        >>> cwd = os.getcwd()
-        >>> download_source(
-                cwd, 
-                'http://data.atoti.io/notebooks/collateral-shortfall-monitoring/assets_positions.csv', 
-                "assets_positions.csv"
-            )
-        assets_positions.csv already downloaded.
+        cwd: current working directory.
+        url: url for the source file
+        filename: filename of the downloaded file
     """
     if os.path.isfile(cwd + "/" + filename):
         print(filename + ' already downloaded.')
