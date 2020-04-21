f3c3aa9cb6aaac4798a79325a67e96043d67d9e5
Yeo Hui Fang <hfy@activeviam.com>
2020-04-21 09:43:28 +0800

    obtain filename from URL instead of parameter input

--- utils/notebook_utils.py
+++ utils/notebook_utils.py
@@ -2,15 +2,15 @@ import wget
 import os.path
 
 # check if file has already been downloaded 
-def download_source(url: str, filename: str):
+def download_source(url: str):
     """This function downloads data file from the given url to the working directory (cwd).
 
     Args:
-        cwd: current working directory.
         url: url for the source file
-        filename: filename of the downloaded file
     """
     cwd = os.getcwd()
+    filename = url.split('/')[-1]
+    
     if os.path.isfile(cwd + "/" + filename):
         print(filename + ' already downloaded.')
     else:
