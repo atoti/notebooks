e2b82d152cb91d2be17e9d3360c3160e3658bf8e
Samir Tazine <gmsamt@gmail.com>
2020-06-09 17:19:21 +0200

    New simulations part

--- retail/geopricing/utils/pricer.py
+++ retail/geopricing/utils/pricer.py
@@ -1,13 +1,37 @@
-def pricing_function(price: float, cluster: int) -> float:
-    if cluster == 0:
-        return price * 1.07
-    elif cluster == 1:
-        return price * 1.08
-    elif cluster == 2:
-        return price * 1.05
-    elif cluster == 3:
-        return price * 1.07
-    elif cluster == 4:
-        return price * 1.07
-    else:
-        return price
+import pandas
+
+
+def optimize_prices(
+    initial_price_list: pandas.DataFrame, store_features_with_clusters: pandas.DataFrame
+) -> pandas.DataFrame:
+    """
+    This pricing function has been made very simple as the main purpose is to serve the use case example.
+    In real life this would typically be replaced by a more flexible price optimization engine with several parameters,
+    such as that of ActiveViam.
+    
+    Args:
+        initial_price_list: The initial SellingPrice list
+        sotre_features_with_clusters: A dataframe containing informations about the stores, including their cluster
+        
+    Returns:
+        A new SellingPrice list with an optimized price stores with low competition have increased prices while those with high competition have competitive prices.
+    """
+    new_price_list = initial_price_list.merge(
+        store_features_with_clusters, left_on="StoreId", right_on="StoreId"
+    )
+    new_price_list.loc[(new_price_list["cluster"] == 0), "SellingPrice"] = (
+        new_price_list["SellingPrice"] * 1.07
+    )
+    new_price_list.loc[(new_price_list["cluster"] == 1), "SellingPrice"] = (
+        new_price_list["SellingPrice"] * 1.08
+    )
+    new_price_list.loc[(new_price_list["cluster"] == 2), "SellingPrice"] = (
+        new_price_list["SellingPrice"] * 1.05
+    )
+    new_price_list.loc[(new_price_list["cluster"] == 3), "SellingPrice"] = (
+        new_price_list["SellingPrice"] * 1.07
+    )
+    new_price_list.loc[(new_price_list["cluster"] == 4), "SellingPrice"] = (
+        new_price_list["SellingPrice"] * 1.07
+    )
+    return new_price_list[["ProductId", "StoreId", "SellingPrice"]]
