50971ecbee66ee02c722c9efc632bab5d167fd91
Yeo Hui Fang <35210495+HuifangYeo@users.noreply.github.com>
2020-07-24 16:02:20 +0800

    Added purchase price and quantity sold (#54)
    - Compute margin instead of using pre-aggregated value
    - fixed shops with 0 competitors within 20km (competitors are further away)

--- retail/geopricing/utils/pricer.py
+++ retail/geopricing/utils/pricer.py
@@ -16,17 +16,14 @@ def optimize_prices(
     Returns:
         A new SellingPrice list with an optimized price stores with low competition have increased prices while those with high competition have competitive prices.
     """
-    
-#     shop_features_with_clusters.reset_index(inplace=True)
-#     shop_features_with_clusters.ShopId = shop_features_with_clusters.ShopId.astype("int64")
 
     new_price_list = initial_price_list.merge(
         shop_features_with_clusters, left_on="ShopId", right_on="ShopId"
     )
-    
+
     new_price_list.loc[(new_price_list["Cluster"] == 0), "SellingPrice"] = (
         new_price_list["SellingPrice"] * 1.02
-    )   
+    )
     new_price_list.loc[(new_price_list["Cluster"] == 1), "SellingPrice"] = (
         new_price_list["SellingPrice"] * 0.95
     )
@@ -35,9 +32,11 @@ def optimize_prices(
     )
     new_price_list.loc[(new_price_list["Cluster"] == 3), "SellingPrice"] = (
         new_price_list["SellingPrice"] * 1.05
-    )    
+    )
     new_price_list.loc[(new_price_list["Cluster"] == 4), "SellingPrice"] = (
         new_price_list["SellingPrice"] * 1.05
     )
-    
-    return new_price_list[["ProductId", "ShopId", "SellingPrice", "Margin"]]
+
+    return new_price_list[
+        ["ProductId", "ShopId", "SellingPrice", "PurchasePrice", "Quantity"]
+    ]
