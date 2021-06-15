bdc1ce1345ea49f73d92ad1b62ae1f52a695a194
bloussou <brieuc.loussouarn@orange.fr>
2021-06-15 12:05:01 +0200

    black on test_notebooks.py

--- .github/workflows/test_notebooks.py
+++ .github/workflows/test_notebooks.py
@@ -17,7 +17,7 @@ NOTEBOOKS_TO_SKIP = [
     "notebooks/customer-churn/main.ipynb",
     "notebooks/customer-churn/1_create_models.ipynb",
     "notebooks/ifrs9/data-generation.ipynb",
-    "notebooks/ifrs9/main.ipynb", # Timeout
+    "notebooks/ifrs9/main.ipynb",  # Timeout
     "notebooks/reddit/main.ipynb",
     "notebooks/twitter/01_tweets_mining.ipynb",
     "notebooks/twitter/02_sentiment.ipynb",
@@ -27,14 +27,20 @@ NOTEBOOKS_TO_SKIP = [
     "notebooks/var-benchmark/main.ipynb",
 ]
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
-notebooks_path = sorted([notebook_path.replace(NOTEBOOKS_FOLDER, "") for notebook_path in glob.glob("notebooks/**/*.ipynb") if notebook_path not in NOTEBOOKS_TO_SKIP])
+logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
+notebooks_path = sorted(
+    [
+        notebook_path.replace(NOTEBOOKS_FOLDER, "")
+        for notebook_path in glob.glob("notebooks/**/*.ipynb")
+        if notebook_path not in NOTEBOOKS_TO_SKIP
+    ]
+)
 
 
 for notebook_path in notebooks_path:
     notebook_path = Path(NOTEBOOKS_FOLDER + notebook_path)
     logging.info(f"Starting execution of {notebook_path}")
     notebook = nbformat.read(notebook_path, as_version=4)
-    ep = ExecutePreprocessor(timeout=300, kernel_name='python3')
-    ep.preprocess(notebook, {'metadata': {'path': notebook_path.parent}})
+    ep = ExecutePreprocessor(timeout=300, kernel_name="python3")
+    ep.preprocess(notebook, {"metadata": {"path": notebook_path.parent}})
     logging.info(f"Execution of {notebook_path} succeed")
