710fa31a731c768c064fe17d01baf12319ad5c39
bloussou <brieuc.loussouarn@orange.fr>
2021-06-15 11:28:07 +0200

    improving execution to have access to utils files

--- .github/workflows/test_notebooks.py
+++ .github/workflows/test_notebooks.py
@@ -1,28 +1,32 @@
 import nbformat
 import glob
 import logging
-import os
 
 from nbconvert.preprocessors import ExecutePreprocessor
+from pathlib import Path
 
+NOTEBOOKS_FOLDER = "notebooks/"
+# List of notebooks failing for different reasons
 NOTEBOOKS_TO_SKIP = [
     "notebooks/sub-population-analysis/main.ipynb",
     "notebooks/real-time-risk/main.ipynb",
     "notebooks/rollup-hierarchies/main.ipynb",
     "notebooks/sbm/main.ipynb",
+    "notebooks/customer-churn/0_prepare_data.ipynb",
+    "notebooks/customer-churn/main.ipynb",
+    "notebooks/customer-churn/1_create_models.ipynb",
+    "notebooks/ifrs9/data-generation.ipynb",
+    "notebooks/ifrs9/main.ipynb", # Timeout
 ]
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
-notebooks_path = [notebook_path for notebook_path in glob.glob("notebooks/**/*.ipynb") if notebook_path not in NOTEBOOKS_TO_SKIP]
+notebooks_path = sorted([notebook_path.replace(NOTEBOOKS_FOLDER, "") for notebook_path in glob.glob("notebooks/**/*.ipynb") if notebook_path not in NOTEBOOKS_TO_SKIP])
 
 
 for notebook_path in notebooks_path:
-    # Cleaning content of temp folder for the next test
-    for temp_files in glob.glob("temp/"):
-        if temp_files != "temp/":
-            os.remove(temp_files)
-    logging.info(f"Start execution of {notebook_path}")
+    notebook_path = Path(NOTEBOOKS_FOLDER + notebook_path)
+    logging.info(f"Starting execution of {notebook_path}")
     notebook = nbformat.read(notebook_path, as_version=4)
-    ep = ExecutePreprocessor(timeout=6000, kernel_name='python3')
-    ep.preprocess(notebook, {'metadata': {'path': 'temp/'}})
+    ep = ExecutePreprocessor(timeout=300, kernel_name='python3')
+    ep.preprocess(notebook, {'metadata': {'path': notebook_path.parent}})
     logging.info(f"Execution of {notebook_path} succeed")
