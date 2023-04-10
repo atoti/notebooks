4d7d9de545c044ef1be9974e373f63e511564131
Yeo Hui Fang <hfy@activeviam.com>
2023-04-10 17:49:52 +0800

    Remove test for food-processing notebook due to mxnet installation issue
    Update sklearn to scikit-learn

--- tests/execute_notebooks.py
+++ tests/execute_notebooks.py
@@ -71,6 +71,7 @@ NOTEBOOKS_WITH_ERRORS = [
     "sbm/main.ipynb",  # broken in 0.6.3 https://github.com/atoti/atoti/issues/413
     f"geopricing/{_MAIN}",  # https://github.com/atoti/notebooks/runs/2829010222 TO FIX,
     # f"collateral-shortfall-forecast/notebooks/{_MAIN}",  # removed tsfresh due to conflict with protobuf
+    "food-processing/main.ipynb" # Validation of the RECORD file of mxnet-1.7.0.post1-py2.py3-none-win_amd64.whl failed
 ]
 NOTEBOOKS_TO_SKIP = sorted(
     DATA_PREPROCESSING_NOTEBOOKS
